3e0d35ce90825e81b9d3b31e637dd30a8cb82412
Minoru Hiki <minoruhiki@gmail.com>
2017-09-01 13:16:59 +0900

    プロパティの例題追加

---
+++ cookbook/9/example_9_21.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+https://learko.github.io/books/coding/Python_Cookbook_3rd_Edition.pdf
+9.21. Avoiding Repetitive Property Methods
+"""
+class Person:
+    def __init__(self, name ,age):
+        self.name = name
+        self.age = age
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError('name must be a string')
+        self._name = value
+
+    @property
+    def age(self):
+        return self._age
+    
+    @age.setter
+    def age(self, value):
+        if not isinstance(value, int):
+            raise TypeError('age must be an int')
+        self._age = value
+
+def typed_property(name, expected_type):
+    storage_name = '_' + name
+    
+    @property
+    def prop(self):
+        print(self)
+        return getattr(self, storage_name)
+    
+    @prop.setter
+    def prop(self, value):
+        if not isinstance(value, expected_type):
+            raise TypeError('{} must be a {}'.format(name, expected_type))
+        setattr(self, storage_name, value)
+
+    return prop
+
+class Person:
+    name = typed_property('name', str)
+    age = typed_property('age', int)
+
+    def __init__(self, name=None, age=None):
+        self.name = name
+        self.age = age
+
+    def read(self);
+
+# Example
+if __name__ == '__main__':
+    a = Person()
+    
+    print(a.name + ' ' + str(a.age))
---
+++ example_discriptor.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+
+class Descriptor:
+    def __init__(self, **opts):
+        self.name = None
+        self.value = None
+        for key, value in opts.items():
+            setattr(self, key, value)
+
+    def __get__(self, instance, type=None):
+        print(instance)
+        return self.value
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+    
+    def read(self):
+        self.value = 'hoge'
+
+class checkedmeta(type):
+    def __new__(cls, clsname, bases, methods):
+        # Attach attribute names to the descriptors
+        for key, value in methods.items():
+            if isinstance(value, Descriptor):
+                value.name = key
+        return type.__new__(cls, clsname, bases, methods)
+
+
+class Test(metaclass=checkedmeta):
+    a = Descriptor()
+    b = Descriptor()
+
+    def read(self):
+        self.a.read()
+        self.b.read()
+
+
+if __name__ == "__main__":
+
+    test = Test()
+    test.read()
+    print(test.a)
